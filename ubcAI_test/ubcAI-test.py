080418c61b856e0cb43392e0b740a88e097db81b
Miao-cc <1033570101@qq.com>
2018-11-20 14:47:34 +0800

    update

--- ubcAI_test/ubcAI-test.py
+++ ubcAI_test/ubcAI-test.py
@@ -1,22 +1,30 @@
 import cPickle, glob, ubc_AI
 import numpy as np
 from ubc_AI.data import pfdreader
-AI_PATH = '/'.join(ubc_AI.__file__.split('/')[:-1])
-#pfd_path = '/data/public/AI_data/pzc_pfds/'
-pfd_path = './'
-#classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_HTRU.pkl','rb'))
-#classifier = cPickle.load(open('../clfl2_PALFA_1.pkl','rb'))
-classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_PALFA.pkl','rb'))
-#classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_BD.pkl','rb'))
-#classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_ResNet.pkl','rb'))
 
-pfdfile = glob.glob(pfd_path + '*.pfd')
-print pfdfile
-AI_scores = classifier.report_score([pfdreader(f) for f in pfdfile])
+def select():
+    AI_PATH = '/'.join(ubc_AI.__file__.split('/')[:-1])
+    #pfd_path = '/data/public/AI_data/pzc_pfds/'
+    pfd_path = './'
+    #classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_HTRU.pkl','rb'))
+    #classifier = cPickle.load(open('../clfl2_PALFA_1.pkl','rb'))
+    classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_PALFA.pkl','rb'))
+    #classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_BD.pkl','rb'))
+    #classifier = cPickle.load(open(AI_PATH+'/trained_AI/clfl2_ResNet.pkl','rb'))
+    
+    pfdfile = glob.glob(pfd_path + '*.pfd')
+    print pfdfile
+    AI_scores = classifier.report_score([pfdreader(f) for f in pfdfile])
+    
+    text = '\n'.join(['%s %s' % (pfdfile[i], AI_scores[i]) for i in range(len(pfdfile))])
+    print text, type(text)
+    fout = open('testResult.txt', 'w')
+    fout.write(text)
+    fout.close()
 
-text = '\n'.join(['%s %s' % (pfdfile[i], AI_scores[i]) for i in range(len(pfdfile))])
-print text, type(text)
-fout = open('testResult.txt', 'w')
-fout.write(text)
-fout.close()
 
+if __name__ == "__main__":
+    try:
+        select()
+    except:
+        print "aa"
